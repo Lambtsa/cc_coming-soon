8a74df08d55a80eb8b23f13df866af05b83142f3
Tom Lamb <lambtsa.tom@gmail.com>
2022-11-22 08:59:02 +0100

    Update to deployment scripts

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -4,8 +4,8 @@ import URL from "@constants/url";
 
 import { ReactComponent as FacebookIcon } from "@assets/icons/facebook.svg";
 import { ReactComponent as InstagramIcon } from "@assets/icons/instagram.svg";
-import { ReactComponent as TiktokIcon } from "@assets/icons/tiktok.svg";
-import { ReactComponent as TwitterIcon } from "@assets/icons/twitter.svg";
+// import { ReactComponent as TiktokIcon } from "@assets/icons/tiktok.svg";
+// import { ReactComponent as TwitterIcon } from "@assets/icons/twitter.svg";
 import { Link } from "@components/Link";
 import { SocialIconsProps, SocialMediaLink } from "./SocialIcons.types";
 
@@ -20,16 +20,16 @@ const socialMediaLinks: SocialMediaLink[] = [
     link: URL.INSTAGRAM,
     svg: <InstagramIcon />,
   },
-  {
-    name: "twitter",
-    link: URL.TWITTER,
-    svg: <TwitterIcon />,
-  },
-  {
-    name: "tiktok",
-    link: URL.TIKTOK,
-    svg: <TiktokIcon />,
-  },
+  // {
+  //   name: "twitter",
+  //   link: URL.TWITTER,
+  //   svg: <TwitterIcon />,
+  // },
+  // {
+  //   name: "tiktok",
+  //   link: URL.TIKTOK,
+  //   svg: <TiktokIcon />,
+  // },
 ];
 
 export const SocialIcons = ({
