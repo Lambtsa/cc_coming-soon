f52866a94c6620dec8b07e47a498e168bb1c3ec9
Tom Lamb <lambtsa.tom@gmail.com>
2022-11-22 14:10:10 +0100

    Update to charlies closet urls

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,8 +1,8 @@
 export default {
   DEV: "http://localhost:3000",
   PROD: "https://www.charlies-closet.com",
-  FACEBOOK: "https://www.facebook.com/charlies-closet",
-  INSTAGRAM: "https://www.instagram.com/charlies-closet",
+  FACEBOOK: "https://www.facebook.com/charliesclosetfr",
+  INSTAGRAM: "https://www.instagram.com/charliesclosetfr/",
   TWITTER: "https://www.charlies-closet.com",
   TIKTOK: "https://www.charlies-closet.com",
 } as const;
